8145a63ce8a8083b4bdd1678994c12edd019b07f
biggicode <larioncescu@yahoo.com>
2022-04-05 14:22:05 +0300

    this is home

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import * as S from "./header.style"
 import Logo from "../logo"
+import Nav from "../nav"
 
 const Header = () => {
   return (
     <S.Header>
       <Logo />
+      <Nav />
     </S.Header>
   )
 }
