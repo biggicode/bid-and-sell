48cd7cb490ec12b5b651f76f243c1bf74482d639
biggicode <larioncescu@yahoo.com>
2022-04-05 14:48:27 +0300

    install react router

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -2,6 +2,9 @@ import { ThemeProvider } from "styled-components"
 import theme from "../theme"
 
 import Header from "../header"
+import Home from "../home"
+import Auctions from "../auctions"
+import Sell from "../sell"
 
 const App = () => {
   return (
