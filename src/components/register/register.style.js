21a5a1193b054e70bd3f701ce563eb85cefe45c3
biggicode <larioncescu@yahoo.com>
2022-04-09 19:40:37 +0300

    add more styles

--- src/components/register/register.style.js
+++ src/components/register/register.style.js
@@ -1,1 +1,11 @@
 import styled from "styled-components"
+
+export const Form = styled.form`
+  border: 2px solid #333;
+  border-radius: 5px;
+  grid-column: span 4;
+`
+
+export const Label = styled.label`
+  color: ${({ theme }) => theme.colors.dark};
+`
