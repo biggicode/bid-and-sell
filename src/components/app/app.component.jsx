53c556248226b954844f9365c2c60c3bcbc42a94
biggicode <larioncescu@yahoo.com>
2022-04-10 22:19:36 +0300

    add provider to app

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "styled-components"
 import theme from "../theme"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "../../redux"
 
 import Header from "../header"
 import Home from "../home"
@@ -13,18 +15,20 @@ import Grid from "../grid-system/grid"
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header />
-        <Grid>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/auctions" element={<Auctions />} />
-            <Route path="/sell" element={<Sell />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Grid>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Grid>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/auctions" element={<Auctions />} />
+              <Route path="/sell" element={<Sell />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Grid>
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   )
 }
