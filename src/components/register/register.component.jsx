05eacccadc1559cb99914d20c485032846ddc066
biggicode <larioncescu@yahoo.com>
2022-04-10 21:15:20 +0300

    add form for registration

--- src/components/register/register.component.jsx
+++ src/components/register/register.component.jsx
@@ -1,7 +1,18 @@
 import * as S from "./register.style"
 
 const Register = () => {
-  return <div>This is register</div>
+  return (
+    <S.Form>
+      <h2>Register!</h2>
+      <S.Label>Email</S.Label>
+      <S.Input type="email" />
+      <S.Label>Passowrd</S.Label>
+      <S.Input type="password" />
+      <S.Label>Password Confirmation</S.Label>
+      <S.Input type="password" />
+      <S.Submit type="submit" text="Register!" />
+    </S.Form>
+  )
 }
 
 export default Register
