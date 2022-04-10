74111b4f83d350398be3e9cc5af3939c6243bda1
biggicode <larioncescu@yahoo.com>
2022-04-10 21:18:14 +0300

    add refs

--- src/components/register/register.component.jsx
+++ src/components/register/register.component.jsx
@@ -1,16 +1,26 @@
+import { useRef } from "react"
 import * as S from "./register.style"
 
 const Register = () => {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+
   return (
     <S.Form>
       <h2>Register!</h2>
       <S.Label>Email</S.Label>
-      <S.Input type="email" />
+      <S.Input type="email" ref={emailRef} required />
       <S.Label>Passowrd</S.Label>
-      <S.Input type="password" />
+      <S.Input type="password" ref={passwordRef} required />
       <S.Label>Password Confirmation</S.Label>
       <S.Input type="password" />
-      <S.Submit type="submit" text="Register!" />
+      <S.Submit
+        type="submit"
+        text="Register!"
+        ref={passwordConfirmRef}
+        required
+      />
     </S.Form>
   )
 }
