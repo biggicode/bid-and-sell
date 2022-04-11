caf83c53df46f5660fc40eba56b6859c1f916552
biggicode <larioncescu@yahoo.com>
2022-04-11 17:57:23 +0300

    add constants for log in

--- src/redux/user.constant.js
+++ src/redux/user.constant.js
@@ -1,7 +1,7 @@
-export const LOGIN = "[user] LOGIN"
-export const LOGOUT = "[user] LOGOUT"
-export const SETUSER = "[user] SETUSER"
-
 export const REGISTER_START = "REGISTER_START"
 export const REGISTER_SUCCES = "REGISTER_SUCCES"
 export const REGISTER_FAIL = "REGISTER_FAIL"
+
+export const LOGIN_START = "LOGIN_START"
+export const LOGIN_SUCCES = "LOGIN_SUCCES"
+export const LOGIN_FAIL = "LOGIN_FAIL"
