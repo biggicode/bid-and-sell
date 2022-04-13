ea308be07b644e3bbdd789f77a676c9152358c55
biggicode <larioncescu@yahoo.com>
2022-04-13 15:18:21 +0300

    create row component

--- src/components/auctions/auctions.component.jsx
+++ src/components/auctions/auctions.component.jsx
@@ -1,6 +1,13 @@
+import Row from "../grid-system/row"
+import Grid from "../grid-system/grid"
+
 const Auctions = () => {
   //TO DO: protect auction id with usefull 1:42
-  return <div>This is auctions</div>
+  return (
+    <Row>
+      <Grid></Grid>
+    </Row>
+  )
 }
 
 export default Auctions
