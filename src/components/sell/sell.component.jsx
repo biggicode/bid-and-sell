96576e2638be58be76157900755788cf4cb88f60
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-04-30 12:20:36 +0300

    add auction duration

--- src/components/sell/sell.component.jsx
+++ src/components/sell/sell.component.jsx
@@ -1,69 +1,71 @@
-import { useState } from "react"
-import { db, storage } from "../../config/firebase"
-import * as S from "./sell.style"
-import { collection, addDoc } from "firebase/firestore"
-import { ref, uploadBytes } from "firebase/storage"
-import { v4 } from "uuid"
+import { useState } from "react";
+import { db, storage } from "../../config/firebase";
+import * as S from "./sell.style";
+import { collection, addDoc } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
 
 const initialValues = {
   auctionTitle: "",
   description: "",
   startingPrice: 10,
   currentPrice: 10,
+  auctionDuration: 1,
   creatorName: "",
   creatorEmail: "",
   phoneNumber: "",
-}
+};
 
 //TO DO: go to read and write settings and allow only register users in firebase (if true)
 
 const Sell = () => {
-  const [state, setState] = useState(initialValues)
-  const [imageUpload, setImageUpload] = useState(null)
+  const [state, setState] = useState(initialValues);
+  const [imageUpload, setImageUpload] = useState(null);
 
   const {
     auctionTitle,
     description,
     startingPrice,
+    auctionDuration,
     creatorName,
     creatorEmail,
     phoneNumber,
-  } = state
+  } = state;
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
 
-    setState({ ...state, [name]: value })
-  }
+    setState({ ...state, [name]: value });
+  };
 
   const handleFileChange = (e) => {
-    setImageUpload(e.target.files[0])
-  }
+    setImageUpload(e.target.files[0]);
+  };
 
-  const uploadImage = () => {}
+  const uploadImage = () => {};
 
   const resetForm = () => {
-    setState(initialValues)
-  }
+    setState(initialValues);
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (imageUpload == null) return
+    if (imageUpload == null) return;
 
-    const imagePath = `images/${imageUpload.name + v4()}`
+    const imagePath = `images/${imageUpload.name + v4()}`;
 
-    const imageRef = ref(storage, imagePath)
-    await uploadBytes(imageRef, imageUpload)
+    const imageRef = ref(storage, imagePath);
+    await uploadBytes(imageRef, imageUpload);
 
     await addDoc(collection(db, "auctions"), {
       ...state,
       currentPrice: state.startingPrice,
       imagePath,
-    })
+    });
 
-    resetForm()
-  }
+    resetForm();
+  };
 
   return (
     <S.Form onSubmit={handleSubmit}>
@@ -99,6 +101,18 @@ const Sell = () => {
         onChange={handleInputChange}
         required
       />
+      <S.Label htmlFor="auctionDuration">Auction duration in hours:</S.Label>
+      <S.Input
+        type="number"
+        name="auctionDuration"
+        id="auctionDuration"
+        step="1"
+        max="24"
+        min="1"
+        value={auctionDuration}
+        onChange={handleInputChange}
+        required
+      />
       <S.Separator />
       <S.SemiTitle>Contact info</S.SemiTitle>
       <S.Label htmlFor="creatorName">Your name</S.Label>
@@ -131,7 +145,7 @@ const Sell = () => {
       <S.Separator />
       <S.Button type="submit">Create auction!</S.Button>
     </S.Form>
-  )
-}
+  );
+};
 
-export default Sell
+export default Sell;
