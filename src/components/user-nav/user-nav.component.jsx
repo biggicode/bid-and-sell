0d1e97fc00e100c22818e89d4ea48bcc99b73618
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-05-07 16:01:11 +0300

    create basic user nav

---
+++ src/components/user-nav/user-nav.component.jsx
@@ -0,0 +1,7 @@
+import * as S from "./user-nav.style";
+
+const UserNav = () => {
+  return <S.DesktopUserNav>User Name</S.DesktopUserNav>;
+};
+
+export default UserNav;
---
+++ src/components/user-nav/user-nav.style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DesktopUserNav = styled.div`
+  position: relative;
+  display: flex;
+  gap: 0.5rem;
+`;
