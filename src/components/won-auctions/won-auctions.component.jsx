5e7c08c0e7f352fee40006f1a1dd4eb5d21b656b
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-05-10 20:33:36 +0300

    get win auctions based on id

--- src/components/won-auctions/won-auctions.component.jsx
+++ src/components/won-auctions/won-auctions.component.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+
+import { db } from "../../config/firebase";
+
 const WonAuctions = () => {
+  const currentUser = useSelector(({ user }) => user.currentUser);
+  const [wonAuctions, setWonAuctions] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "finished"),
+      where("winnerId", "==", currentUser.uid)
+    );
+
+    onSnapshot(q, (querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        list.push(doc.data());
+      });
+
+      setWonAuctions(list);
+    });
+  }, []);
+
+  console.log(wonAuctions);
+
   return <div>Won auctions</div>;
 };
 
