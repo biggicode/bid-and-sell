c33a0dde42f9f6faf500534ed35d1c6c462215b1
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-05-11 20:44:10 +0300

    create querys

--- src/components/my-auctions/my-auctions.component.jsx
+++ src/components/my-auctions/my-auctions.component.jsx
@@ -1,8 +1,25 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+
+import { db } from "../../config/firebase";
 
 const MyAuctions = () => {
   const currentUser = useSelector(({ user }) => user.currentUser);
+  const [liveAuctions, setLiveAuctions] = useState([]);
+  const [finishedAuctions, setFinishedAuctions] = useState([]);
+
+  useEffect(() => {
+    const liveQ = query(
+      collection(db, "auctions"),
+      where("creatorId", "==", currentUser.uid)
+    );
+
+    const finishedQ = query(
+      collection(db, "finished"),
+      where("creatorId", "==", currentUser.uid)
+    );
+  }, []);
 
   return <div>my auctions</div>;
 };
