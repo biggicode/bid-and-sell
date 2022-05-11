92601c6d428d443e308d9e3886c379146f78b298
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-05-11 20:31:24 +0300

    get current user

--- src/components/my-auctions/my-auctions.component.jsx
+++ src/components/my-auctions/my-auctions.component.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
 const MyAuctions = () => {
+  const currentUser = useSelector(({ user }) => user.currentUser);
+
   return <div>my auctions</div>;
 };
 
