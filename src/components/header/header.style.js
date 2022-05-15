c4f5bfe4a69334e3a2a00ccdd14a94f8a6d63e6e
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-05-15 15:06:18 +0300

    nav fixed, font and bgc

--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 import { Link as RouterLink } from "react-router-dom";
 
 export const Header = styled.header`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
   height: 64px;
   padding: 0 1rem;
   background-color: ${({ theme }) => theme.colors.primary};
--- src/components/theme/theme.config.js
+++ src/components/theme/theme.config.js
@@ -2,20 +2,21 @@ const colors = {
   light: "#F7F7FF",
   danger: "#6AED12",
   warning: "#EDDF12",
-  primary: "#4D13ED",
+  primary: "#1D1D26",
   primaryDarker: "#3D0FBE",
   dark: "#090909",
-}
+  border: "#707070",
+};
 
 const breakpoints = {
   mobile: "768px",
   tablet: "992px",
   desktop: "1200px",
-}
+};
 
 const theme = {
   colors,
   breakpoints,
-}
+};
 
-export default theme
+export default theme;
