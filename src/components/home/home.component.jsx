f9be2f29e5c3e1a0451a67483dcc3e5849e42a16
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-05-16 23:54:07 +0300

    make circle component reusable

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,5 +1,13 @@
-const Home = () => {
-  return <div>This is home</div>
-}
+import CircleGradient from "../circle-gradient";
 
-export default Home
+const Home = () => {
+  return (
+    <div>
+      This is home
+      <CircleGradient top="300px" type="black" />
+      <CircleGradient top="20px" right="50px" />
+    </div>
+  );
+};
+
+export default Home;
